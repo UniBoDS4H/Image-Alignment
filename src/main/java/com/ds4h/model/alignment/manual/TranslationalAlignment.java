476b6256e7fad320facd8cff04d0252f4f22117f
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2023-03-07 11:21:16 +0100

    trying to create ransac

--- src/main/java/com/ds4h/model/alignment/manual/TranslationalAlignment.java
+++ src/main/java/com/ds4h/model/alignment/manual/TranslationalAlignment.java
@@ -8,8 +8,10 @@ import org.opencv.core.*;
 import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 /**
@@ -33,6 +35,16 @@ public class TranslationalAlignment extends AlignmentAlgorithm {
     @Override
     protected Optional<AlignedImage> align(final ImagePoints targetImage, final ImagePoints imagePoints) throws IllegalArgumentException{
         try {
+            MatOfPoint2f tarO = new MatOfPoint2f();
+            MatOfPoint2f srcO = new MatOfPoint2f();
+            ransac(imagePoints.getMatOfPoint(), targetImage.getMatOfPoint(), 100,10,srcO, tarO);
+            //Arrays.stream(targetImage.getPoints()).forEach(targetImage::removePoint);
+            //Arrays.stream(imagePoints.getPoints()).forEach(imagePoints::removePoint);
+            System.out.println(srcO.toList());
+            System.out.println(tarO.toList());
+            srcO.toList().forEach(imagePoints::addPoint);
+            tarO.toList().forEach(targetImage::addPoint);
+
             if(targetImage.numberOfPoints() >= LOWER_BOUND && imagePoints.numberOfPoints() >= LOWER_BOUND) {
                 final Mat targetMat = targetImage.getMatImage();
                 final Mat imageToShiftMat = imagePoints.getMatImage();
@@ -57,6 +69,79 @@ public class TranslationalAlignment extends AlignmentAlgorithm {
             throw ex;
         }
     }
+    public void ransac(MatOfPoint2f src, MatOfPoint2f dst, int num, double threshold, MatOfPoint2f inliersSrc, MatOfPoint2f inliersDst) {
+
+        int numPoints = src.rows();
+
+
+        // Initialize variables
+        Random rand = new Random();
+        int maxInliers = 0;
+        MatOfPoint2f bestInliersSrc = new MatOfPoint2f();
+        MatOfPoint2f bestInliersDst = new MatOfPoint2f();
+
+        // Run RANSAC for a fixed number of iterations
+        for (int iter = 0; iter < num; iter++) {
+            System.out.println("numPoints->"+ numPoints);
+            // Select a random sample of correspondences
+            int[] indices = rand.ints(numPoints, 0, numPoints).distinct().limit(2).toArray();
+            Arrays.stream(indices).forEach(System.out::println);
+            System.out.println();
+            src.toList().forEach(System.out::println);
+            System.out.println();
+
+
+            // Compute the homography using the random sample
+            MatOfPoint2f srcSample = new MatOfPoint2f();
+            MatOfPoint2f dstSample = new MatOfPoint2f();
+            for (int i = 0; i < 2; i++) {
+                srcSample.push_back(src.row(indices[i]));
+                dstSample.push_back(dst.row(indices[i]));
+            }
+            Mat H = getMat(srcSample, dstSample);
+
+            // Compute the inliers using the homography and the threshold
+            MatOfPoint2f inliersSrcTmp = new MatOfPoint2f();
+            MatOfPoint2f inliersDstTmp = new MatOfPoint2f();
+            for (int i = 0; i < numPoints; i++) {
+                Mat srcPoint = src.row(i);
+                Mat dstPoint = dst.row(i);
+                double[] srcArray = srcPoint.get(0, 0);
+                double[] dstArray = dstPoint.get(0, 0);
+                MatOfPoint2f srcMat = new MatOfPoint2f(new Point(srcArray));
+                MatOfPoint2f dstMat = new MatOfPoint2f(new Point(dstArray));
+                MatOfPoint2f dstTransformed = new MatOfPoint2f();
+                Core.perspectiveTransform(srcMat, dstTransformed, H);
+                double distance = Core.norm(dstMat, dstTransformed);
+                if (distance < threshold) {
+                    inliersSrcTmp.push_back(srcMat);
+                    inliersDstTmp.push_back(dstMat);
+                }
+            }
+
+            // Update the best set of inliers
+            int numInliers = (int) inliersSrcTmp.total();
+            if (numInliers > maxInliers) {
+                maxInliers = numInliers;
+                bestInliersSrc = inliersSrcTmp;
+                bestInliersDst = inliersDstTmp;
+            }
+        }
+        System.out.println("best"+ bestInliersSrc.toList());
+
+        // Return the best set of inliers
+        inliersSrc.fromList(bestInliersSrc.toList());
+        inliersDst.fromList(bestInliersDst.toList());
+    }
+    public Mat getMat(final MatOfPoint2f imageToAlign, final MatOfPoint2f targetImage){
+        final Point translation = minimumLeastSquare(imageToAlign.toArray(), targetImage.toArray());
+        final Mat translationMatrix = Mat.eye(3, 3, CvType.CV_32FC1);
+        translationMatrix.put(0, 2, translation.x);
+        translationMatrix.put(1, 2, translation.y);
+        //super.addMatrix(imageToAlign, translationMatrix);
+        return translationMatrix;
+    }
+
 
     @Override
     public Mat getTransformationMatrix(final ImagePoints imageToAlign, final ImagePoints targetImage){
